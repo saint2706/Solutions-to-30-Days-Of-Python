1728ef0bd0172f5ee61b8c4d053b6dbeb2bb984d
SAIN'T <arishabh748@gmail.com>
2021-08-21 18:40:39 +0530

    Create variables.py

---
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,56 @@
+# Day 2: 30 Days of python programming
+
+# Level 1
+first_name = 'Rishabh'
+last_name = 'Agrawal'
+full_name = first_name + ' ' + last_name
+country = 'India'
+city = 'Navi Mumbai'
+age = 19
+year = 2021
+is_married = False
+is_true = True
+is_light_on = True
+language, email = 'English', 'arishabh748@gmail.com'
+
+# Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(language))
+print(type(email))
+
+print(len(first_name))
+
+print(len(first_name) > len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = 30
+area_of_circle = 3.14*radius*radius
+circum_of_circle = 3.14*2*radius
+radius = int(input('Enter radius:'))
+area_of_circle = 3.14*radius*radius
+circum_of_circle = 3.14*2*radius
+
+first = input('Enter first name:')
+last = input('Enter last name:')
+country = input('Enter country:')
+age = int(input('Enter age:'))
+
+help('keywords')
