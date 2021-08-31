41e6d6b63f0c3ec2869ffdaf1eaa1a1562a6fc25
SAIN'T <arishabh748@gmail.com>
2021-08-31 11:05:25 +0530

    Update conditionals.py

--- 09_Day_Conditionals/conditionals.py
+++ 09_Day_Conditionals/conditionals.py
@@ -60,3 +60,32 @@ fruits = ['banana', 'orange', 'mango', 'lemon']
 fruit = input('Enter fruit: ')
 print('That fruit already exists in the list' if fruit in fruits else fruits.append(fruit))
 print(fruits)
+
+# Level 3
+person={
+'first_name': 'Asabeneh',
+'last_name': 'Yetayeh',
+'age': 250,
+'country': 'Finland',
+'is_marred': True,
+'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+'address': {
+    'street': 'Space street',
+    'zipcode': '02210'
+}
+}
+
+if person['skills']:
+    print(person['skills'][len(person['skills'])//2])
+    print("Python" in person['skills'])
+    if ['Javascript', 'React'] == person['skills']:
+        print('Front End Developer')
+    elif ['Node', 'MongoDB', 'React'] == person['skills']:
+        print('Full Stack Developer')
+    else:
+        print("Unknown Title")
+
+if person['is_marred']:
+    print(person['first_name'], person['last_name'], "lives in", person['country'], ". He is married")
+else:
+    print(person['first_name'], person['last_name'], "lives in", person['country'], ". He is not married")
