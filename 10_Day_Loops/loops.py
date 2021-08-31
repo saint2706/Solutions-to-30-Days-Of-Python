58979e9d8b9ac038ce6f59e6dd9728ff1fff2e02
SAIN'T <arishabh748@gmail.com>
2021-08-31 11:12:26 +0530

    Create loops.py

---
+++ 10_Day_Loops/loops.py
@@ -0,0 +1,19 @@
+# Level 1
+# 1
+for i in range(0, 11):
+    print(i)
+
+k = 0
+while k <=10:
+    print(k)
+    k+=1
+
+# 2
+for i in range(10, -1, -1):
+    print(i)
+
+k = 10
+while k >=0:
+    print(k)
+    k-=1
+
