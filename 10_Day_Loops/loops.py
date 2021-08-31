e6280f63327ad2b6d9ba72f1d532abbe78e86fda
SAIN'T <arishabh748@gmail.com>
2021-08-31 11:25:12 +0530

    Update loops.py

--- 10_Day_Loops/loops.py
+++ 10_Day_Loops/loops.py
@@ -17,3 +17,50 @@ while k >=0:
     print(k)
     k-=1
 
+# 3
+hash_string = '#'
+for i in range(1, 8):
+    print(hash_string*i)
+
+# 4
+for i in range(1, 9):
+    for j in range(1, 9):
+        print("#", end=' ')
+    print()
+
+# 5
+for i in range(0, 11):
+    print(i, "x", i, "=", i*i)
+
+# 6
+for lang in ['Python', 'Numpy','Pandas','Django', 'Flask']:
+    print(lang)
+
+# 7
+for i in range(0, 101):
+    if i%2 == 0:
+        print(i)
+
+# 8
+for i in range(0, 101):
+    if i%2 != 0:
+        print(i)
+
+# Level 2
+# 1
+sum = 0
+for i in range(0, 101):
+    sum+=i
+print("The sum of all numbers is", sum)
+
+# 2
+even_sum = 0
+odd_sum = 0
+for i in range(0, 101):
+    if i%2 == 0:
+        even_sum+= i
+    else:
+        odd_sum+=i
+print("The sum of all even numbers is", even_sum)
+print("The sum of all odd numbers is", odd_sum)
+
