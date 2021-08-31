59e065d94aade27b32d03122387343d617c77943
SAIN'T <arishabh748@gmail.com>
2021-08-31 11:55:47 +0530

    Update loops.py

--- 10_Day_Loops/loops.py
+++ 10_Day_Loops/loops.py
@@ -86,5 +86,19 @@ print(rev)
 list_data = countries_data.data
 total_langs = []
 for i in list_data:
-    total_langs.append(i["languages"])
-print("Languages = ", len(list(set(total_langs))))
+    total_langs.extend(i["languages"])
+print("Languages = ", len(set(total_langs)))
+counts = {}
+for i in total_langs:
+    counts[i] = counts.get(i, 0) + 1
+def sort_dict_by_value(d, reverse = False):
+    return dict(sorted(d.items(), key = lambda x:x[1], reverse = reverse))
+counts = sort_dict_by_value(counts, True)
+for i in list(counts.items())[:10]:
+    print(i)
+populations = {}
+for i in list_data:
+    populations[i["name"]] = i["population"]
+populations = sort_dict_by_value(populations, True)
+for i in list(populations.items())[:10]:
+    print(i)
