e3e7574fd46facae225e6f1219522f617abbcb70
SAIN'T <arishabh748@gmail.com>
2021-10-15 11:29:59 +0530

    Update regex.py

--- 18_Day_Regular_Expressions/regex.py
+++ 18_Day_Regular_Expressions/regex.py
@@ -3,7 +3,8 @@ import string
 from collections import Counter
 from pprint import pprint
 
-paragraph = "I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love."
+paragraph = "I love teaching. If you do not love teaching what else can you love. I love Python if you do not love " \
+            "something which can give you all the capabilities to develop an application what else can you love. "
 
 
 def most_common_words(text):
@@ -14,7 +15,8 @@ def most_common_words(text):
 
 
 pprint(most_common_words(paragraph))
-para = """The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction."""
+para = """The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 
+0 at origin, 4 and 8 in the positive direction. """
 
 num_list = list(map(int, re.findall(r"[-+]?[.]?[\d]+", para)))
 num_list.sort()
@@ -33,12 +35,15 @@ def is_valid_variable(potential_variable):
 pprint(is_valid_variable("first_name"))
 pprint(is_valid_variable("first-name"))
 
-sentence = """%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?"""
+sentence = """%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as 
+educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s 
+mo@tivate yo@u to be a tea@cher!? """
 
 
+# noinspection DuplicatedCode
 def clean_text(text):
     text = text.lower()
-    text = re.sub("\[.*?\]", "", text)
+    text = re.sub("\[.*?]", "", text)
     text = re.sub("https?://\S+|www\.\S+", "", text)
     text = re.sub("<.*?>+", "", text)
     text = re.sub("[%s]" % re.escape(string.punctuation), "", text)
