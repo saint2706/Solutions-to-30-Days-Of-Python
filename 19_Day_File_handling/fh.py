46779f61c44a0db389265e12d6a940e999672657
SAIN'T <arishabh748@gmail.com>
2021-10-15 10:05:06 +0530

    major changes to countries data

--- 19_Day_File_handling/fh.py
+++ 19_Day_File_handling/fh.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 
 def counter(fname):
     num_words = 0
@@ -28,3 +28,21 @@ counter("data\donald_speech.txt")
 print()
 print("Melania Trump:")
 counter("data\melina_trump_speech.txt")
+
+f = open('data\countries_data.json', encoding='UTF8')
+work = json.load(f)
+total_languages_initial = []
+for i in work:
+    total_languages_initial.extend(i["languages"])
+
+
+counts = {}
+for i in total_languages_initial:
+    counts[i] = counts.get(i, 0) + 1
+def sort_dict_by_value(d, reverse=False):
+    return dict(sorted(d.items(), key=lambda x: x[1], reverse=reverse))
+
+
+counts = sort_dict_by_value(counts, True)
+for i in list(counts.items())[:20]:
+    print(i)
