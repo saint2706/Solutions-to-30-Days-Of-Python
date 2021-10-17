cf1f3641e9f12035f2a1e9da228964162a661db1
SAIN'T <arishabh748@gmail.com>
2021-10-17 20:39:48 +0530

    Update CaO.py

--- 21_Day_Classes_and_objects/CaO.py
+++ 21_Day_Classes_and_objects/CaO.py
@@ -58,3 +58,27 @@ class PersonAccount:
         self.lastname = lastname
         self.incomes = incomes
         self.expenses = expenses
+    def total_income(self):
+        return sum(self.incomes.values())
+    def total_expense(self):
+        return sum(self.expenses.values())
+    def account_info(self):
+        return "%s %s is the person's name. Their income is %d and their expense is %d." % (self.firstname, self.lastname, self.total_income(), self.total_expense())
+    def add_income(self, data):
+        for k,v in data.items():
+            self.incomes[k] = v
+        return self.incomes
+    def add_expense(self, data):
+        for k,v in data.items():
+            self.expenses[k] = v
+        return self.expenses
+    def account_balance(self):
+        return self.total_income() - self.total_expense()
+
+me = PersonAccount('Rishabh', 'Agrawal', {'Salary': 150000, 'Bonus': 5500}, {'Rent': 20000, 'General': 4500})
+print(me.total_income())
+print(me.total_expense())
+print(me.account_info())
+print(me.add_income({'Diwali': 2500, 'Birthday': 5000}))
+print(me.add_expense({'Tax': 2500, 'Fuel': 5000}))
+print(me.account_balance())
