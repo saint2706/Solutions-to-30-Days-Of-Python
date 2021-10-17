a8502eb8b0acdecb2bb0b589f2ef0fa0fdc2a021
SAIN'T <arishabh748@gmail.com>
2021-10-17 22:52:00 +0530

    Yay
    Cheeky Workaround lmao

--- 22_Day_Web_scraping/url2.py
+++ 22_Day_Web_scraping/url2.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import csv
 import json
+from os import remove
 
 url = 'https://archive.ics.uci.edu/ml/datasets.php'
 df = pd.read_html(requests.get(url).content)[5]
@@ -26,3 +27,4 @@ def make_json(csvFilePath, jsonFilePath):
 csvFilePath = r'22_Day_Web_scraping\scrapped_data.csv'
 jsonFilePath = r'22_Day_Web_scraping\scrapped_datasets.json'
 make_json(csvFilePath, jsonFilePath)
+remove(r'22_Day_Web_scraping\scrapped_data.csv')
