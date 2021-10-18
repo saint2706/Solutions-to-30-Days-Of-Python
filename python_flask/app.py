a63ea28beef09b77aa3f9cc3795d664abdf20c00
SAIN'T <arishabh748@gmail.com>
2021-10-18 18:11:17 +0530

    yolo

--- python_flask/app.py
+++ python_flask/app.py
@@ -15,7 +15,7 @@ def home():
 
 @app.route('/about')
 def about():
-    name = '30 Days Of Python Programming'
+    name = 'Rishabh Agrawal'
     return render_template('about.html', name=name, title='About Us')
 
 
