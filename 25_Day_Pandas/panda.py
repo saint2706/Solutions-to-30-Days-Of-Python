fae129adb5d1f9a69109d99ec4463cdb27ca3fce
SAIN'T <arishabh748@gmail.com>
2021-10-18 17:59:26 +0530

    Update panda.py

--- 25_Day_Pandas/panda.py
+++ 25_Day_Pandas/panda.py
@@ -3,7 +3,20 @@ import pandas as pd
 df = pd.read_csv(r'data\hacker_news.csv')
 
 first_5_rows = df.head()
-print(first_5_rows)
+# print(first_5_rows)
 
 last_5_rows = df.tail()
-print(last_5_rows)
+# print(last_5_rows)
+
+title_series = df.iloc[:,2]
+# print(title_series)
+
+# print(df.shape)
+
+python_title = df.loc[df['title'].str.contains('Python', case=False)]
+# print(python_title)
+
+js_title = df.loc[df['title'].str.contains('JavaScript', case=False)]
+# print(js_title)
+
+# print(df.describe())
