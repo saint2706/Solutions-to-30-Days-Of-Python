b02dd7759751c84b256ad3437cfd5cca06babcc8
Saint <arishabh748@gmail.com>
2025-10-14 19:58:49 +0530

    Delete site directory

--- site/jupyterlite/files/Day_31_Databases/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Day 31 database lesson package."""
-
-from .databases import (  # noqa: F401
-    EMPLOYEE_ROWS,
-    cleanup_employee_db,
-    fetch_department_dataframe,
-    fetch_department_salaries,
-    initialize_employee_db,
-)
